74c05f773424f0e9c01d33e3128edaabe685602f
Abhinav Agarwal <abhinavagarwalsps@gmail.com>
2021-08-18 23:07:12 +0530

    Initial Commit
    Signed-off-by: Abhinav Agarwal <abhinavagarwalsps@gmail.com>

---
+++ main.py
@@ -0,0 +1,30 @@
+# Morse code dictionary
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-',' ':'.......'}
+
+# Function to encrypt text to morse code
+def encrypt(text):
+    morseCode = ''
+    for i in text:
+        if i in MORSE_CODE_DICT:
+            morseCode += MORSE_CODE_DICT[i]+' '
+    return morseCode
+
+# Driver code
+if __name__ == "__main__":
+    string = input("Enter the text: ")
+    morse = encrypt(string.upper())
+    print(morse)
